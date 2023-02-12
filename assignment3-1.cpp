b6dad93ac4ed44834ab8b849fc3669220be46f64
Coastpure <njorogesimon465@gmail.com>
2023-02-12 21:59:58 +0300

    first commit

---
+++ assignment3-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float math, physics, chemistry, average_score;
+
+    math = 94;
+    physics = 85;
+    chemistry = 87;
+
+    average_score = average_score =(math +physics + chemistry)/3;
+
+    cout << "Your Math score is\n" << math;
+    cout << "Your physics score is\n" << physics;
+    cout << "And your chemistry score is\n" <<chemistry;
+    cout << "Your average score is\n" <<average_score;
+}
